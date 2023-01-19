d2c4829473f8b3261665db1125c368a2da75d2cc
Ramon Pawlowski <ramonpaw@gmail.com>
2023-01-19 07:21:17 +0100

    Initial

---
+++ Core/Objects/Cube.cs
@@ -0,0 +1,27 @@
+﻿using System.Numerics;
+using Raylib_CsLo;
+using static Raylib_CsLo.Raylib;
+
+
+namespace Core.Objects;
+
+public class Cube
+{
+        public Vector3 Size;
+        public Vector3 Position;
+
+        public Cube(Vector3 position, Vector3 size)
+        {
+                Position = position;
+                Size = new Vector3(size.X, size.Y, size.Z);
+                DrawCube(position, Size.X, Size.Y, Size.Z, BLUE);
+        }
+}
+
+public static class CubeExtensions
+{
+        public static void Outline(this Cube cube, Color color)
+        {
+                DrawCubeWires(cube.Position, cube.Size.X, cube.Size.Y, cube.Size.Z, color);
+        }
+}
