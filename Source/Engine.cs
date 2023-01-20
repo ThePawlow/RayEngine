5459c948926273e51e9f77b087a99f71c6db800e
ramon.pawlowski <ramon.pawlowski@ise.de>
2023-01-20 10:07:26 +0100

    Edited Objects.Cube

--- Source/Engine.cs
+++ Source/Engine.cs
@@ -1,76 +1,74 @@
 ﻿using System.Numerics;
+using RayEngine.Objects;
 using Raylib_CsLo;
 using static Raylib_CsLo.Raylib;
 
-namespace RayEngine
+namespace RayEngine;
+
+public static class Engine
 {
-        public static class Engine
+        public static unsafe int Main()
         {
-                public static unsafe int Main()
+                // Initialization
+                InitWindow(1240, 800, "RayEngine");
+
+                // Define the camera to look into our 3d world
+                Camera3D camera = new()
                 {
-                        // Initialization
-                        InitWindow(1240, 800, "Игровой движок");
+                        position = new(10.0f, 10.0f, 10.0f),// Camera position
+                        target = new(0.0f, 0.0f, 0.0f),// Camera looking at point
+                        up = new(0.0f, 1.0f, 0.0f),// Camera up vector (rotation towards target)
+                        fovy = 45.0f,// Camera field-of-view Y
+                        projection_ = CameraProjection.CAMERA_PERSPECTIVE// Camera mode type
+                };
 
-                        // Define the camera to look into our 3d world
-                        Camera3D camera = new()
+                SetCameraMode(camera, CameraMode.CAMERA_FREE);// Set a free camera mode
+
+                SetCameraPanControl((int) MouseButton.MOUSE_BUTTON_RIGHT);
+
+                SetTargetFPS(60);// Set our game to run at 60 frames-per-second
+
+                // Main game loop
+                while (!WindowShouldClose())// Detect window close button or ESC key
+                {
+                        // Update
+                        UpdateCamera(&camera);// Update camera
+
+                        if (IsKeyDown('Z'))
                         {
-                                position = new(10.0f, 10.0f, 10.0f),// Camera position
-                                target = new(0.0f, 0.0f, 0.0f),// Camera looking at point
-                                up = new(0.0f, 1.0f, 0.0f),// Camera up vector (rotation towards target)
-                                fovy = 45.0f,// Camera field-of-view Y
-                                projection_ = CameraProjection.CAMERA_PERSPECTIVE// Camera mode type
-                        };
-
-                        SetCameraMode(camera, CameraMode.CAMERA_FREE);// Set a free camera mode
-
-                        SetCameraPanControl((int) MouseButton.MOUSE_BUTTON_RIGHT);
-
-                        SetTargetFPS(60);// Set our game to run at 60 frames-per-second
-
-                        // Main game loop
-                        while (!WindowShouldClose())// Detect window close button or ESC key
-                        {
-                                // Update
-                                UpdateCamera(&camera);// Update camera
-
-                                if (IsKeyDown('Z'))
-                                {
-                                        camera.target = new(0.0f, 0.0f, 0.0f);
-                                }
-
-                                // Draw
-                                BeginDrawing();
-
-                                ClearBackground(RAYWHITE);
-
-                                BeginMode3D(camera);
-
-                                DrawCube(Vector3.Zero, 2.0f, 2.0f, 2.0f, RED);
-                                DrawCubeWires(Vector3.Zero, 2.0f, 2.0f, 2.0f, MAROON);
-                                // var cube = new Cube(Vector3.Zero, new Vector3(2f, 2f, 2f));
-                                // cube.Outline(DARKBLUE);
-
-                                DrawGrid(32, 1.0f);
-
-                                EndMode3D();
-
-                                DrawRectangle(10, 10, 320, 133, Fade(SKYBLUE, 0.5f));
-                                DrawRectangleLines(10, 10, 320, 133, BLUE);
-
-                                DrawText("Free camera default controls:", 20, 20, 10, BLACK);
-                                DrawText("- Mouse Wheel to Zoom in-out", 40, 40, 10, DARKGRAY);
-                                DrawText("- Mouse Right Pressed to Pan", 40, 60, 10, DARKGRAY);
-                                DrawText("- Alt + Mouse Right Pressed to Rotate", 40, 80, 10, DARKGRAY);
-                                DrawText("- Alt + Ctrl + Mouse Right Pressed for Smooth Zoom", 40, 100, 10, DARKGRAY);
-                                DrawText("- Z to zoom to (0, 0, 0)", 40, 120, 10, DARKGRAY);
-
-                                EndDrawing();
+                                camera.target = Vector3.Zero;
                         }
 
+                        // Draw
+                        BeginDrawing();
 
-                        CloseWindow();// Close window and OpenGL context
+                        ClearBackground(RAYWHITE);
 
-                        return 0;
+                        BeginMode3D(camera);
+
+                        // Gets replaced by Objects.Cube
+                        var cube = new Cube(Vector3.Zero, new Vector3(2f, 2f, 2f), DARKBLUE);
+
+                        DrawGrid(32, 1.0f);
+
+                        EndMode3D();
+
+                        DrawRectangle(10, 10, 320, 133, Fade(SKYBLUE, 0.5f));
+                        DrawRectangleLines(10, 10, 320, 133, BLUE);
+
+                        DrawText("Free camera default controls:", 20, 20, 10, BLACK);
+                        DrawText("- Mouse Wheel to Zoom in-out", 40, 40, 10, DARKGRAY);
+                        DrawText("- Mouse Right Pressed to Pan", 40, 60, 10, DARKGRAY);
+                        DrawText("- Alt + Mouse Right Pressed to Rotate", 40, 80, 10, DARKGRAY);
+                        DrawText("- Alt + Ctrl + Mouse Right Pressed for Smooth Zoom", 40, 100, 10, DARKGRAY);
+                        DrawText("- Z to zoom to (0, 0, 0)", 40, 120, 10, DARKGRAY);
+
+                        EndDrawing();
                 }
+
+
+                CloseWindow();// Close window and OpenGL context
+
+                return 0;
         }
 }
--- Source/Objects/Cube.cs
+++ Source/Objects/Cube.cs
@@ -3,25 +3,28 @@ using Raylib_CsLo;
 using static Raylib_CsLo.Raylib;
 
 
-namespace RayEngineNet7.Objects;
+namespace RayEngine.Objects;
 
 public class Cube
 {
-        public Vector3 Size;
-        public Vector3 Position;
+        /// <summary>
+        /// Readonly, as there's no DrawPool yet
+        /// </summary>
+        public readonly Vector3 Size;
+        
+        /// <summary>
+        /// Readonly, as there's no DrawPool yet
+        /// </summary>
+        public readonly Vector3 Position;
 
-        public Cube(Vector3 position, Vector3 size)
+        public Cube(Vector3 position, Vector3 size, Color color, bool outlined = true)
         {
                 Position = position;
                 Size = new Vector3(size.X, size.Y, size.Z);
-                DrawCube(position, Size.X, Size.Y, Size.Z, BLUE);
-        }
-}
-
-public static class CubeExtensions
-{
-        public static void Outline(this Cube cube, Color color)
-        {
-                DrawCubeWires(cube.Position, cube.Size.X, cube.Size.Y, cube.Size.Z, color);
+                DrawCube(position, Size.X, Size.Y, Size.Z, color);
+                if (outlined)
+                {
+                        DrawCubeWires(Position, Size.X, Size.Y, Size.Z, BLACK);
+                }
         }
 }
